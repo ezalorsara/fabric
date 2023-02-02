92c8335dc61fc4017e0b88adbd89680136e2922b
Joed Parin <parinjoed@gmail.com>
2023-02-02 10:45:31 +0800

    test

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -2,12 +2,14 @@ import React, { useRef, useEffect, useState } from "react";
 import { fabric } from "fabric";
 import { PDFDocument } from "pdf-lib";
 import { Document, Page, pdfjs } from "react-pdf";
+import * as htmlToImage from "html-to-image";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const Canvas: React.FC = () => {
   const [base64, setBase64] = useState("");
   const canvasRef = useRef<fabric.Canvas | null>(null);
+  const divRef = useRef<HTMLDivElement | null>(null);
   const doPdf = async () => {
     const pdfDoc = await PDFDocument.create();
     const page = pdfDoc.addPage();
@@ -40,12 +42,24 @@ const Canvas: React.FC = () => {
 
   return (
     <div>
-      <canvas id="canvas" width={500} height={500} />
+      <div ref={divRef}>
+        <canvas id="canvas" width={500} height={500} />
+      </div>
       {!!base64 && (
-        <Document file={base64} onLoadSuccess={() => {}}>
+        <Document
+          file={base64}
+          onLoadSuccess={async () => {
+            if (divRef.current) {
+              const dataUrl = await htmlToImage.toPng(divRef.current, {
+                quality: 1,
+              });
+            }
+          }}
+        >
           <Page pageNumber={1} />
         </Document>
       )}
+      <button onClick={() => {}}>download</button>
     </div>
   );
 };
