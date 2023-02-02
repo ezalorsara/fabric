5231b3b95d59ef69f4394679aa23888b38c8aacb
Joed Parin <parinjoed@gmail.com>
2023-02-02 09:29:55 +0800

    test

--- next.config.js
+++ next.config.js
@@ -12,6 +12,14 @@ if (
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      fabric: "fabric-pure-browser",
+    };
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
